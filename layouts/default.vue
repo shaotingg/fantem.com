0a9fe647b95f9efd5836e661727ec372a9eceaa5
Lerte Smith <smith.shi@fantem.com>
2018-01-24 09:22:15 +0800

    prettify scrollbar
    Signed-off-by: Lerte Smith <smith.shi@fantem.com>

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,14 @@
 <template>
   <nuxt/>
 </template>
+
+<style lang="stylus">
+::-webkit-scrollbar
+  width: 8px
+  &-thumb
+    background: #999
+    &:hover
+      background: #666
+  &-track-piece
+    background: #fff
+</style>
